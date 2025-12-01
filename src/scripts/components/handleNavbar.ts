905954e39802f636c05dc10fe61a1471929628b1
Jordy756 <jordycastro1756@gmail.com>
2025-12-01 01:51:40 -0600

    refactor(4G-0): update button and link components for improved accessibility and styling

--- src/scripts/components/handleNavbar.ts
+++ src/scripts/components/handleNavbar.ts
@@ -1,25 +1,37 @@
 export const initNavbar = () => {
     const nav = document.querySelector("#navbar") as HTMLElement;
+
+    if (window.innerWidth > 864) return { nav };
+
     const menuBtn = nav.querySelector("#menu-btn") as HTMLButtonElement;
     const navbarLinks = nav.querySelectorAll("ul > li > a, div > a") as NodeListOf<HTMLAnchorElement>;
     const overlay = document.querySelector("#overlay") as HTMLDivElement;
 
-    const setupMobileMenu = (): void => {
-        if (window.innerWidth > 864) return;
-
-        const addToggleListener = (element: HTMLElement) =>
-            element.addEventListener("click", () => nav.classList.toggle("open"));
-
-        const addRemoveListener = (element: HTMLElement) =>
-            element.addEventListener("click", () => nav.classList.contains("open") && nav.classList.remove("open"));
-
-        addToggleListener(menuBtn);
-        addRemoveListener(overlay);
-
-        navbarLinks.forEach(addRemoveListener);
+    const toggleAriaExpanded = () => {
+        const isExpanded = menuBtn.getAttribute("aria-expanded") === "true";
+        menuBtn.setAttribute("aria-expanded", String(!isExpanded));
+        menuBtn.setAttribute("aria-label", isExpanded ? "Abrir menú de navegación" : "Cerrar menú de navegación");
     };
 
-    setupMobileMenu();
+    const addToggleListener = (element: HTMLElement) => {
+        element.addEventListener("click", () => {
+            nav.classList.toggle("open");
+            toggleAriaExpanded();
+        });
+    };
+
+    const addRemoveListener = (element: HTMLElement) => {
+        element.addEventListener("click", () => {
+            if (!nav.classList.contains("open")) return;
+
+            nav.classList.remove("open");
+            toggleAriaExpanded();
+        });
+    };
+
+    addToggleListener(menuBtn);
+    addRemoveListener(overlay);
+    navbarLinks.forEach(addRemoveListener);
 
     return { nav };
 };
