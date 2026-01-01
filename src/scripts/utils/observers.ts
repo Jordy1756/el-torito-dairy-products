682188c4ba490e5bba19546d93d32bec4689001f
Jordy756 <jordycastro1756@gmail.com>
2026-01-01 02:01:56 -0600

    style(4G-0): update overflow property to contain in AboutUs.astro

--- src/scripts/utils/observers.ts
+++ src/scripts/utils/observers.ts
@@ -1,5 +1,5 @@
 export const createBannerObserver = (nav: HTMLElement): IntersectionObserver => {
-    const options = {
+    const options: IntersectionObserverInit = {
         root: null,
         rootMargin: "-1px",
         threshold: 0,
@@ -9,14 +9,17 @@ export const createBannerObserver = (nav: HTMLElement): IntersectionObserver => 
 };
 
 export const createSectionObserver = (sectionId: string): IntersectionObserver => {
+    const viewportHeight = window.innerHeight;
+    const topMargin = Math.floor(viewportHeight * 0.3);
+
     const options = {
         root: null,
-        rootMargin: "-1px",
-        threshold: 0.35,
+        rootMargin: `-${topMargin}px 0px -${topMargin}px 0px`,
+        threshold: 0.1,
     };
 
     return new IntersectionObserver(([entry]) => {
-        const navigationItem = document.querySelector(`li[data-link="${sectionId}"] > a`) as HTMLAnchorElement;;
+        const navigationItem = document.querySelector(`li[data-link="${sectionId}"] > a`) as HTMLAnchorElement;
         navigationItem.classList.toggle("active", entry.isIntersecting);
     }, options);
 };
